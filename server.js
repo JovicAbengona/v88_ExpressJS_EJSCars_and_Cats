34bf60963b850b49fa0ba7d5d3a705082e48e2c9
JovicAbengona <37503727+JovicAbengona@users.noreply.github.com>
2021-05-24 13:42:02 +0800

    First push for ExpressJS Cars and Cats assignment using EJS

---
+++ server.js
@@ -0,0 +1,33 @@
+var express = require("express");
+var app = express();
+
+app.use(express.static(__dirname + "/static"));
+
+// This sets the location where express will look for the ejs views
+app.set('views', __dirname + '/views'); 
+// Now lets set the view engine itself so that express knows that we are using ejs as opposed to another templating engine like jade
+app.set('view engine', 'ejs');
+
+app.get("/", function(request, response){
+    response.sendFile(path.join(__dirname, "static", "index.html"));
+});
+
+app.get("/cars", function(request, response){
+    response.render('cars');
+});
+
+app.get("/cars/new", function(request, response){
+    response.render('form');
+});
+
+app.get("/cats", function(request, response){
+    response.render('cats');
+});
+
+app.use(function(request, response){
+    response.render('404');
+});
+
+app.listen(8080, function(){
+    console.log("Listening on 8080");
+});
